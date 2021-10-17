58ca91b27578630d1642b70af09f5224d88c7bba
enKODING <65354945+enKODING1@users.noreply.github.com>
2021-10-17 10:22:09 +0900

    stack: solved

--- 스택/3 9012 괄호/app.c
+++ 스택/3 9012 괄호/app.c
@@ -1,24 +1,119 @@
 #include <stdio.h>
+#define MAX_SIZE 50
 
 int count = -1;
 
-void push(int *target, int item)
+void init()
 {
-    target[++count] = item;
+    count = -1;
 }
 
-void pop(int *target)
+void push(char *target, char item)
 {
-    int temp = target[count];
-    count -= 1;
-    return temp;
+    count++;
+    target[count] = item;
+}
+
+int isEmpty()
+{
+    if (count < 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+char pop(char *target)
+{
+    if (isEmpty() == 1)
+    {
+        return 'n';
+    }
+    else
+    {
+        char temp = target[count];
+        count -= 1;
+        return temp;
+    }
+}
+
+char top(char *target)
+{
+    if (isEmpty() == 1)
+    {
+        return 'n';
+    }
+    else
+    {
+        return target[count];
+    }
+}
+
+int strLen(char *str)
+{
+    int len = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        len = i + 1;
+    }
+
+    return len;
+}
+
+void pushVPS(char *vps_data)
+{
+    int flag = 0;
+    int len = strLen(vps_data);
+
+    for (int i = 0; i < len; i++)
+    {
+        if (vps_data[i] == '(')
+        {
+            push(vps_data, vps_data[i]);
+        }
+        else if (vps_data[i] == ')')
+        {
+            if (top(vps_data) == '(')
+            {
+                pop(vps_data);
+            }
+            else if (top(vps_data) == 'n')
+            {
+                flag = 1;
+            }
+        }
+    }
+
+    if (count == -1 && flag == 1)
+    {
+        printf("NO\n");
+    }
+    else if (count == -1)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+
+    init();
 }
 
 int main(void)
 {
-    int arr_size = 0;
-    scanf("%d", &arr_size);
+    char vps[MAX_SIZE];
+    int loop = 0;
+    scanf("%d", &loop);
 
-    printf("%d", count);
+    for (int i = 0; i < loop; i++)
+    {
+        scanf("%s", vps);
+        pushVPS(vps);
+    }
+
     return 0;
 }
