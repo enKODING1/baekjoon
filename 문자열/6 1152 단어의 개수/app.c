44e02b6c4ef986e47ecf2569570554fad08835b4
enKODING <65354945+enKODING1@users.noreply.github.com>
2021-12-31 01:06:27 +0900

    solved

---
+++ 문자열/6 1152 단어의 개수/app.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX_SIZE 1000000
+
+typedef struct text
+{
+    char data[MAX_SIZE];
+    int len;
+    int count;
+} text;
+
+int checkText(text data)
+{
+    if (data.data[0] != ' ')
+        data.count++;
+
+    for (int i = 0; i < data.len; i++)
+    {
+        if (data.data[i] == ' ' && data.data[i + 1] != ' ' && data.data[i + 1] != '\n')
+        {
+
+            data.count++;
+        }
+    }
+
+    if (data.data[data.len - 1] == ' ')
+        data.count--;
+
+    return data.count;
+}
+
+int main(void)
+{
+    text t1;
+    t1.len = 0;
+    t1.count = 0;
+
+    scanf("%[^\n]", t1.data);
+    t1.len = strlen(t1.data);
+
+    printf("%d", checkText(t1));
+
+    return 0;
+}
