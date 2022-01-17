62cf86d4bc31616258978889da31c1a7fbfacefe
enKODING <65354945+enKODING1@users.noreply.github.com>
2022-01-17 23:18:39 +0900

    c solved

--- 대회/2021 POSTECH Programming Open Contest/회전/app.c
+++ 대회/2021 POSTECH Programming Open Contest/회전/app.c
@@ -3,14 +3,14 @@
 #include <string.h>
 #define BUF_SIZE 15
 
-int solve(unsigned long long int num_data, char *num_str);
-int add(unsigned long long int num, int len);
+int solve(long long int num_data, char *num_str);
+int add(long long int num, int len);
 
 int main(void)
 {
-    unsigned long long int num = 0;
+    long long int num = 0;
     int num_length = 0;
-    unsigned long long int total = 0;
+    long long int total = 0;
     scanf("%d", &num);
     char buf[BUF_SIZE];
     sprintf(buf, "%d", num);
@@ -22,20 +22,20 @@ int main(void)
         total += num;
     }
 
-    printf("%d\n", total);
+    printf("%lld\n", total);
     return 0;
 }
 
-int solve(unsigned long long int num_data, char *num_str)
+int solve(long long int num_data, char *num_str)
 {
     int num_length = strlen(num_str);
-    unsigned long long int num1 = add(num_data % 10, num_length);
-    unsigned long long int num2 = num_data / 10;
+    long long int num1 = add(num_data % 10, num_length);
+    long long int num2 = num_data / 10;
 
     return num1 + num2;
 }
 
-int add(unsigned long long int num, int len)
+int add(long long int num, int len)
 {
     for (int i = 0; i < len - 1; ++i)
     {
@@ -43,14 +43,3 @@ int add(unsigned long long int num, int len)
     }
     return num;
 }
-
-// #include <stdio.h>
-
-// int main(void)
-// {
-//     unsigned long long int num = 987654321;
-//     // int num = 9000000000;
-//     printf("%u", num);
-
-//     return 0;
-// }
